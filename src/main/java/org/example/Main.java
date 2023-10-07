05075e848a2bd7ee832bd4cea93f695f63924b9e
tanzilaziim <tanzilrxbow@gmail.com>
2023-10-08 01:32:47 +0700

    initial project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        Buku a = new Buku();
-//        a.setNilai("Jurassic Park", "Michael Crichton", 21);
-//        a.cetakKeLayar();
-        Buku a,b = new Buku();
-        a = new Buku("Siaga Merah", "Alistair Maclean");
-        b = new Buku();
+        Buku a = new Buku();
+        a.setNilai("Jurassic Park", "Michael Crichton", 21);
         a.cetakKeLayar();
-        System.out.println();
-        b.cetakKeLayar();
     }
 }
